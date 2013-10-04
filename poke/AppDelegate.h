eccb696f0097e7823faf819412c6f0345173ac0b
Tóth Csaba <huncsacsika@gmail.com>
2013-10-04 17:35:59 +0200

    Initial Commit

---
+++ poke/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  poke
+//
+//  Created by Toth Csaba on 10/4/13.
+//  Copyright (c) 2013 Distinction. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ poke/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  poke
+//
+//  Created by Toth Csaba on 10/4/13.
+//  Copyright (c) 2013 Distinction. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
