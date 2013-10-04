76f044680ca0095c0cff10208be19ffc8462f294
Tóth Csaba <huncsacsika@gmail.com>
2013-10-04 19:38:21 +0200

    Revert e380093..bd81ec0
    This rolls back to commit e38009374aa7c5f3c895835ff4662c40d5fb64cb.

--- poke/RootViewController.m
+++ poke/RootViewController.m
@@ -8,7 +8,6 @@
 
 #import "RootViewController.h"
 #import "MainTableCell.h"
-#import <Parse/Parse.h>
 
 @interface RootViewController ()
 
@@ -20,29 +19,17 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        [self initParse];
         // Custom initialization
         [self.view setBackgroundColor:[UIColor redColor]];
         tbl = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
         [tbl setDataSource:self];
+        [tbl setContentInset:UIEdgeInsetsMake(60, 0, 0, 0)];
         [tbl  setDelegate:self];
         [self.view addSubview:tbl];
         [tbl registerClass:[MainTableCell class] forCellReuseIdentifier:@"mainCell"];
         list = @[@"elso",@"masodik",@"harmadik"].mutableCopy;
     }
     return self;
-}
--(void)initParse
-{
-    [Parse setApplicationId:@"7rie26iRzAwT5W1AlPweu6hFcIbqRT5DT9cmWJQb" clientKey:@"sk4dkLvdjQALhli0O1cPjU2Wpqng28e0Fnc1Q3wr"];
-    
-    NSError* error;
-    [PFUser logInWithUsername:@"testuser" password:@"123456" error:&error];
-    
-    if (error) {
-        NSLog(@"Cant login to parse:%@",[error localizedDescription]);
-    }
-
 }
 
 - (void)viewDidLoad
@@ -71,5 +58,10 @@
     [cell setText:list[indexPath.row]];
     
     return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"tableview selected:%d",indexPath.row);
 }
 @end
