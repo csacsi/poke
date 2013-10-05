44c589c47a2e0a56795b77082193dbf8220e6959
csomakk <csomakk@Toths-Mac-mini.local>
2013-10-05 02:10:17 +0200

    added Message to Model

---
+++ poke/Data/Model/Message.h
@@ -0,0 +1,23 @@
+//
+//  Message.h
+//  poke
+//
+//  Created by Csomakk on 10/5/13.
+//  Copyright (c) 2013 Distinction. All rights reserved.
+//
+
+#import "BaseDataObject.h"
+
+@interface Message : BaseDataObject
+
+@property (nonatomic,strong)NSString* objectId;
+@property BOOL cancelled;
+@property NSDate *date;
+@property BOOL isSMS;
+@property BOOL sent;
+@property NSString *text;
+@property NSString *to;
+@property NSNumber *userid;
+
+
+@end
