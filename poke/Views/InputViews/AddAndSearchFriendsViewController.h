25031162d172064aa6f80f000b8b95cc7ae395fd
Tóth Csaba <huncsacsika@gmail.com>
2013-10-05 02:50:11 +0200

    added view controller for search and stuff

---
+++ poke/Views/InputViews/AddAndSearchFriendsViewController.h
@@ -0,0 +1,14 @@
+//
+//  AddAndSearchFriendsViewController.h
+//  poke
+//
+//  Created by Toth Csaba on 10/5/13.
+//  Copyright (c) 2013 Distinction. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddAndSearchFriendsViewController : UIViewController
+- (IBAction)backPressed:(id)sender;
+
+@end
---
+++ poke/Views/InputViews/AddAndSearchFriendsViewController.m
@@ -0,0 +1,42 @@
+//
+//  AddAndSearchFriendsViewController.m
+//  poke
+//
+//  Created by Toth Csaba on 10/5/13.
+//  Copyright (c) 2013 Distinction. All rights reserved.
+//
+
+#import "AddAndSearchFriendsViewController.h"
+
+@interface AddAndSearchFriendsViewController ()
+
+@end
+
+@implementation AddAndSearchFriendsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)backPressed:(id)sender {
+    [[NSNotificationCenter defaultCenter]postNotificationName:eventMainViewBackToNormal object:self.view];
+}
+
+@end
